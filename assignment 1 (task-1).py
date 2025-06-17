eda6749bdf8bbd88b698a769a92613b8f9774c92
GOVIND SHARMA <workwithe11even@gmail.com>
2025-06-17 15:02:53 +0530

    Assignment-1

---
+++ assignment 1 (task-1).py
@@ -0,0 +1,8 @@
+from ftplib import parse229
+
+p = int(input('p: '))
+n = int(input('n: '))
+print("addition : ", int(p+n))
+print("subtraction : ", int(p-n))
+print("multiplication : ", int(p*n))
+print("division : ", float(p/n))
---
+++ assignment-1 (task 2).py
@@ -0,0 +1,3 @@
+p=input("Enter your first name : ")
+n=input("Enter your last name : ")
+print("Hello ," , p+(n+"!"),"Welcome to the python program.")
